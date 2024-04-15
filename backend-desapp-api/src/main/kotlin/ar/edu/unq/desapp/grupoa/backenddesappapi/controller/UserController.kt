ade5fd1e05f1c640ec9ee933f2e0d3e0b8484aa3
jaraJulieta <jarajuli13@gmail.com>
2024-04-15 01:40:59 -0300

    user registration restrictions

--- backend-desapp-api/src/main/kotlin/ar/edu/unq/desapp/grupoa/backenddesappapi/controller/UserController.kt
+++ backend-desapp-api/src/main/kotlin/ar/edu/unq/desapp/grupoa/backenddesappapi/controller/UserController.kt
@@ -3,14 +3,17 @@ package ar.edu.unq.desapp.grupoa.backenddesappapi.controller
 import ar.edu.unq.desapp.grupoa.backenddesappapi.controller.DTO.UserDTO
 import ar.edu.unq.desapp.grupoa.backenddesappapi.model.User
 import ar.edu.unq.desapp.grupoa.backenddesappapi.service.UserService
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@Validated
 class UserController (private val userService: UserService){
     @PostMapping("/register")
-    fun register(@RequestBody userData: UserDTO): ResponseEntity<String> {
+    fun register(@Valid @RequestBody userData: UserDTO): ResponseEntity<String> {
         val newUser = User(
             name = userData.name,
             email = userData.email,
